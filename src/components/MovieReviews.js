eb2fa3c7228cd684aa6dcf04a23a4a18d0e93c36
EliLauff <eli.lauffenburger@gmail.com>
2019-04-26 11:12:05 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,16 @@
 // Code MovieReviews Here
+import React, { Component } from "react";
+
+const MovieReviews = props => {
+  console.log(props);
+  console.log(props.display_title);
+  return (
+    <div>
+      <h4>{props.headline}</h4>
+      <h6>{props.byline}</h6>
+      <p>{props.summary_short}</p>
+      <br />
+    </div>
+  );
+};
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,20 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
-import MovieReviews from './MovieReviews'
+import React, { Component } from "react";
+import "isomorphic-fetch";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const NYT_API_KEY = "vnC1SNKGIeopyKiELHnGYDZmGa7KvZFj";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+  `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+  state = { reviews: "", searchTerm: "" };
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        I'm the SearchableMovieReviewsContainer.
+      </div>
+    );
+  }
+}
